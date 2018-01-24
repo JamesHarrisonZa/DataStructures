11a9135d16853579cd3f68e97685985a1ce59b96
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-24 15:14:22 +0200

    added Node class

---
+++ src/node.spec.js
@@ -0,0 +1,17 @@
+const Node = require('./node');
+const assert = require('assert');
+
+describe('A Node,', () => {
+
+	describe('set with a given value,', () => {
+
+		let actual;
+		const expected = 42;
+
+		before(() => {
+			actual = new Node(42).value;
+		});
+
+		it('should return that value', () => assert.equal(actual, expected));
+	});
+});
