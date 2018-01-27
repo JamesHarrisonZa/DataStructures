e8ffa1bc9670edc971c70a2bb1201149677c453f
James Harrison <James.Harrison.Za@gmail.com>
2018-01-27 15:40:54 +0200

    moved setup logic into the "it" block as it is the SUT

--- src/listNode.spec.js
+++ src/listNode.spec.js
@@ -4,23 +4,36 @@ const ListNode = require('./listNode');
 const assert = require('assert');
 
 describe('A ListNode,', () => {
+	
 	describe('set with a given value,', () => {
-		const actual = new ListNode(42).value;
-		const expected = 42;
-		it('should return the value', () => assert.equal(actual, expected));
+		describe('.value, ', () => {
+			it('should return that value', () => {
+				const actual = new ListNode(42).value;
+				const expected = 42;
+				assert.equal(actual, expected)
+			});
+		});
 	});
 
 	describe('set with a next Node,', () => {
-		const nextNode = new ListNode(1, null, null);
-		const actual = new ListNode(0, nextNode).next;
-		const expected = nextNode;
-		it('should return the next Node', () => assert.equal(actual, expected));
+		describe('.next, ', () => {
+			it('should return the next Node', () => {
+				const nextNode = new ListNode(1, null, null);
+				const actual = new ListNode(0, nextNode).next;
+				const expected = nextNode;
+				assert.equal(actual, expected)
+			});
+		});
 	});
 
 	describe('set with a previous Node,', () => {
-		const previousNode = new ListNode(-1, null, null);
-		const actual = new ListNode(0, null, previousNode).previous;
-		const expected = previousNode;
-		it('should return the previous Node', () => assert.equal(actual, expected));
+		describe('.next, ', () => {
+			it('should return the previous Node', () => {
+				const previousNode = new ListNode(-1, null, null);
+				const actual = new ListNode(0, null, previousNode).previous;
+				const expected = previousNode;
+				assert.equal(actual, expected)
+			});
+		});
 	});
 });
