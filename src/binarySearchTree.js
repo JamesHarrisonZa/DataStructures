eaba763ed79f8b96ecc96f244756ba17cfa55e2c
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-02-02 12:24:59 +0200

    Refactoring

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -50,40 +50,23 @@ class BinarySearchTree {
 	 * @param {TreeNode} currentNode
 	 */
 	remove(value, currentNode) {
-		if (!this.root) 
-			return this;
-		else if (value === this.root.value)
-			this.root = null;
-		else if (!currentNode)
-			return this.remove(value, this.root);
-		else if (isNodeToRemove(currentNode.right, value)) {
-			if (!hasChildren(currentNode.right))
-				currentNode.right = null;
-			else {
-				if (!currentNode.right.right)
-					currentNode.right = currentNode.right.left;
-				else
-					currentNode.right = currentNode.right.right;
-			}
-			this.count--;
-		} 
-		else if (isNodeToRemove(currentNode.left, value)) {
-			if (!hasChildren(currentNode.left))
-				currentNode.left = null;
-			else {
-				if (!currentNode.left.right)
-					currentNode.left = currentNode.left.left;
-				else
-					currentNode.left = currentNode.left.right;
-			}
-			this.count--;
-		}
+
+		if (!this.root) return this;
+		else if (value === this.root.value) this.root = null;
+		else if (!currentNode) return this.remove(value, this.root);
+
+		else if (isNodeToRemove(currentNode.left, value))
+			performRemoveLeft(this, currentNode);
+		else if (isNodeToRemove(currentNode.right, value))
+			performRemoveRight(this, currentNode);
+		
 		else if (value > currentNode.value) {
 			if (currentNode.right)
 				return this.remove(value, currentNode.right);
 		}
 		else if (value < currentNode.value) {
-			return this.remove(value, currentNode.left);
+			if (currentNode.left)
+				return this.remove(value, currentNode.left);
 		}
 		return this;
 	}
@@ -92,5 +75,31 @@ class BinarySearchTree {
 const isNodeToRemove = (node, value) => node && value === node.value;
 
 const hasChildren = (node) => node.left || node.right;
+
+const performRemoveLeft = (tree, currentNode) => {
+
+	if (!hasChildren(currentNode.left))
+		currentNode.left = null;
+	else {
+		if (!currentNode.left.right)
+			currentNode.left = currentNode.left.left;
+		else
+			currentNode.left = currentNode.left.right;
+	}
+	tree.count--;
+};
+
+const performRemoveRight = (tree, currentNode) => {
+
+	if (!hasChildren(currentNode.right))
+		currentNode.right = null;
+	else {
+		if (!currentNode.right.right)
+			currentNode.right = currentNode.right.left;
+		else
+			currentNode.right = currentNode.right.right;
+	}
+	tree.count--;
+};
 
 module.exports = BinarySearchTree;
