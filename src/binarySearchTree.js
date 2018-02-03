7695ef48a33f345813494cfeeebc74d95a0aa7b5
James Harrison <James.Harrison.Za@gmail.com>
2018-02-03 17:57:41 +0200

    Extracted function getNextBiggest

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -9,32 +9,32 @@ class BinarySearchTree {
 	constructor(...nodes) {
 		this.root;
 		this.count = 0;
-		
+
 		for (const node of nodes) {
 			this.insert(node);
 		}
 	}
-	
+
 	/**
 	 * @param {number} value
 	 * @param {TreeNode} currentNode
 	 */
 	insert(value, currentNode) {
-		if (!this.root) 
+		if (!this.root)
 			performInsert(this, value);
-		else if (!currentNode) 
+		else if (!currentNode)
 			return this.insert(value, this.root);
-			
+
 		else if (value > currentNode.value) {
-			if (!currentNode.right) 
+			if (!currentNode.right)
 				performInsert(this, value, currentNode);
-			else 
+			else
 				return this.insert(value, currentNode.right);
-		} 
+		}
 		else if (value < currentNode.value) {
 			if (!currentNode.left)
 				performInsert(this, value, currentNode, true);
-			else 
+			else
 				return this.insert(value, currentNode.left);
 		}
 		return this;
@@ -46,18 +46,18 @@ class BinarySearchTree {
 	 */
 	remove(value, currentNode) {
 
-		if (!this.root) 
+		if (!this.root)
 			return this;
-		else if (value === this.root.value) 
+		else if (value === this.root.value)
 			this.root = null;
-		else if (!currentNode) 
+		else if (!currentNode)
 			return this.remove(value, this.root);
 
 		else if (isNodeToRemove(currentNode.left, value))
 			performRemove(this, currentNode, true);
 		else if (isNodeToRemove(currentNode.right, value))
 			performRemove(this, currentNode);
-		
+
 		else if (value > currentNode.value) {
 			if (currentNode.right)
 				return this.remove(value, currentNode.right);
@@ -73,7 +73,7 @@ class BinarySearchTree {
 }
 
 const performInsert = (tree, value, parentNode, left) => {
-	
+
 	(!tree.root)
 		? tree.root = new TreeNode(value)
 		: (left)
@@ -90,44 +90,50 @@ const hasOneChild = (node) => (node.left && !node.right) || (!node.left && node.
 
 const performRemove = (tree, parentNode, left) => {
 
-	const currentNode = (left) ? parentNode.left: parentNode.right;
+	const currentNode = (left) ? parentNode.left : parentNode.right;
 
-	if (hasNoChildren(currentNode)){
-		(left) 
+	if (hasNoChildren(currentNode)) {
+		(left)
 			? parentNode.left = null
 			: parentNode.right = null;
 	}
 	else if (hasOneChild(currentNode)) {
 		if (!currentNode.right)
-			(left) 
+			(left)
 				? parentNode.left = parentNode.left.left
 				: parentNode.right = parentNode.right.left;
 		else
-			(left) 
+			(left)
 				? parentNode.left = parentNode.left.right
 				: parentNode.right = parentNode.right.right;
 	}
 	else { //hasTwoChildren
 		if (!currentNode.right.left)
-			(left) 
+			(left)
 				? parentNode.left = parentNode.left.right
 				: parentNode.right = parentNode.right.right;
 		else {
-			let previousNode;
-			let leftMostNode = currentNode.right.left;
-			while (leftMostNode.left) {
-				previousNode = leftMostNode;
-				leftMostNode = leftMostNode.left;
-			}
-			if (previousNode) previousNode.left = null;
-			leftMostNode.left = currentNode.left;
-			leftMostNode.right = currentNode.right;
-			(left) 
+			let leftMostNode = getNextBiggest(currentNode);
+			(left)
 				? parentNode.left = leftMostNode
 				: parentNode.right = leftMostNode;
 		}
 	}
 	tree.count--;
 };
+
+const getNextBiggest = (currentNode) => {
+	let previousNode;
+	let leftMostNode = currentNode.right.left;
+	while (leftMostNode.left) {
+		previousNode = leftMostNode;
+		leftMostNode = leftMostNode.left;
+	}
+	if (previousNode)
+		previousNode.left = null;
+	leftMostNode.left = currentNode.left;
+	leftMostNode.right = currentNode.right;
+	return leftMostNode;
+}
 
 module.exports = BinarySearchTree;
